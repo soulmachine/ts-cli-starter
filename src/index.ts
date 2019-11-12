c42a17da65b2f1241659479880fd0018cce69e84
soulmachine <soulmachine@gmail.com>
2019-11-11 22:35:26 -0800

    1.o

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import yargs from 'yargs';
+import chalk from 'chalk';
+import figlet from 'figlet';
+
+const argv = yargs.options({
+  a: { type: 'boolean', default: false },
+  b: { type: 'string', demandOption: true },
+  c: { type: 'number', alias: 'chill' },
+  d: { type: 'array' },
+  e: { type: 'count' },
+  f: { choices: ['1', '2', '3'] },
+}).argv;
+
+console.log(chalk.green(figlet.textSync('ts-cli-starter')));
+
+console.log('Hello World');
+
+console.info(argv);
+
+console.info(chalk.green('Green text'));
